98a47b629c9445cebebd943a4b752a24f23fc6b0
aaryaparulekar25-svg <aaryaparulekar25@gmail.com>
2025-12-02 12:54:50 +0530

    chore(firebase): deduplicate initialization; export single initializer from client/src/firebase.js

--- client/gita_admin_panel/admin-client-firebase/src/firebase/firebase.js
+++ client/gita_admin_panel/admin-client-firebase/src/firebase/firebase.js
@@ -1,27 +1,8 @@
-import { initializeApp } from "firebase/app";
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  addDoc,
-  deleteDoc,
-  doc,
-  updateDoc,
-} from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+// Re-export the canonical Firebase app and services from the main initializer
+// at `client/src/firebase.js` to avoid duplicate initialization.
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-};
+import app, { auth, db } from '../../../../src/firebase';
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+export { app, auth, db };
 
 export default app;
