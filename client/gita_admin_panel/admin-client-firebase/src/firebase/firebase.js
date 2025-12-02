52a0784a0d073fe0d6ed686844e6560370b7503a
aaryaparulekar25-svg <aaryaparulekar25@gmail.com>
2025-12-02 12:59:32 +0530

    Final cleanup - ensure single firebase initializer

--- client/gita_admin_panel/admin-client-firebase/src/firebase/firebase.js
+++
@@ -1,8 +0,0 @@
-// Re-export the canonical Firebase app and services from the main initializer
-// at `client/src/firebase.js` to avoid duplicate initialization.
-
-import app, { auth, db } from '../../../../src/firebase';
-
-export { app, auth, db };
-
-export default app;
--- client/gita_admin_panel/admin-client-firebase/src/firebase/helpers.js
+++
@@ -1,8 +0,0 @@
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "./firebase";
-
-export const fetchCollection = async (collectionName) => {
-  const ref = collection(db, collectionName);
-  const snapshot = await getDocs(ref);
-  return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-};
